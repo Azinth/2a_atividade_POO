ee697977a62c79805c3b35a86bc2303b9f96da09
Azinth <gsv.jesus@gmail.com>
2022-04-09 17:20:21 -0300

    if_add

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,14 @@ const prompt = require("prompt-sync")();
 function somaDosN (num1:int,num2:int){
 
   let soma = 0;
-  for (let i = (num1+1); i < num2; i++) {
-    soma += i;
+  if (num1<num2) {
+    for (let i = (num1+1); i < num2; i++) {
+      soma += i;}
+  } else {
+    for (let i = (num2+1); i < num1; i++) {
+      soma += i;
+  }
+
   }
   return soma;
 };
