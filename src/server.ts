16a5accf9f3768ec1481064eab8c42175b657613
Azinth <gsv.jesus@gmail.com>
2022-04-09 18:58:59 -0300

    second_question

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+//1ª questão
+
 const prompt = require("prompt-sync")();
 
 function somaDosN (num1:int,num2:int){
@@ -19,3 +21,29 @@ let a = parseInt(prompt("digite o primeiro número positivo: "))
 let b = parseInt(prompt("digite o segundo número positivo: "))
 
 console.log(`a soma dos números inteiros no intervalo entre os dois números é igual a ${somaDosN(a,b)}`);
+
+//2ª questão
+
+class Pessoa{
+  nome: string;
+  idade: number;
+
+  constructor(nome:string, idade: number){
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+   idadeMeses(): void{
+     this.idade = idade*12
+     console.log(`você tem ${this.idade} meses de idade`)
+   }
+
+}
+
+let nomeP = prompt('digite seu nome: ')
+let idade = parseInt(prompt('digite a sua idade: '))
+
+let pessoa = new Pessoa(nomeP, idade) //esse new serve para adicionar o novo objeto, pq podem haver outros.
+
+console.log(`olá ${nomeP}!`)
+pessoa.idadeMeses()
