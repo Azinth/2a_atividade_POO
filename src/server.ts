2e16da2b16ecbcbc09df65b7a0545c5b44fb8749
Azinth <gsv.jesus@gmail.com>
2022-04-09 17:10:30 -0300

    first_commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+function somaDosN (num1:int,num2:int){
+
+  let soma = 0;
+  for (let i = (num1+1); i < num2; i++) {
+    soma += i;
+  }
+  return soma;
+};
+
+let a = parseInt(prompt("digite o primeiro número positivo: "))
+let b = parseInt(prompt("digite o segundo número positivo: "))
+
+console.log(`a soma dos números inteiros no intervalo entre os dois números é igual a ${somaDosN(a,b)}`);
